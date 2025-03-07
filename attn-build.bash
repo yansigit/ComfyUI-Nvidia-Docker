8ef8e8a0a08b8e9d01868bf76cebc019f0ff689f
Aiden Yoon <44089734+yansigit@users.noreply.github.com>
2025-03-07 01:53:47 -0700

    add an option to use nightly pytorch

--- attn-build.bash
+++ attn-build.bash
@@ -7,9 +7,14 @@ python3 -m venv venv
 . venv/bin/activate
 pip3 install setuptools wheel packaging
 
-# pip3 install --pre torch torchaudio torchvision --index-url https://download.pytorch.org/whl/nightly/cu128
-pytorch_line=$(curl -s https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/scripts/install-comfyui-venv-linux.sh | grep 'torch torchvision torchaudio')
-eval "$pytorch_line"
+if [ "$PYTORCH_VERSION" = "nightly" ]; then
+  echo "Installing nightly PyTorch..."
+  pip3 install --upgrade --pre torch torchaudio torchvision --index-url https://download.pytorch.org/whl/nightly/cu128
+else
+  echo "Installing PyTorch version from ComfyUI Manager repo..."
+  pytorch_line=$(curl -s https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/scripts/install-comfyui-venv-linux.sh | grep 'torch torchvision torchaudio')
+  eval "$pytorch_line"
+fi
 
 pip3 install -e .
 python setup.py bdist_wheel
--- attn.bash
+++ attn.bash
@@ -1,6 +1,11 @@
 echo "== Activating virtua lenv"
 source /comfy/mnt/venv/bin/activate
 
+if [ "$PYTORCH_VERSION" = "nightly" ]; then
+  echo "== Installing nightly torch from https://download.pytorch.org/whl/nightly/cu128"
+  pip3 install --upgrade --pre torch torchaudio torchvision --index-url https://download.pytorch.org/whl/nightly/cu128
+fi
+
 # Install whl files under /builds directory
 echo "== Installing whl files under /builds directory"
 pip3 install /builds/*.whl
