9ea0b359159553039744bd78b94b6d975fee0d89
Aiden Yoon <44089734+yansigit@users.noreply.github.com>
2025-03-08 00:06:29 -0700

    update

--- attn-build.bash
+++ attn-build.bash
@@ -10,6 +10,9 @@ pip3 install setuptools wheel packaging
 if [ "$PYTORCH_VERSION" = "nightly" ]; then
   echo "Installing nightly PyTorch..."
   pip3 install --upgrade --pre torch torchaudio torchvision --index-url https://download.pytorch.org/whl/nightly/cu128
+elif [ -n "$PYTORCH_VERSION" ]; then
+  echo "== Installing torch version $PYTORCH_VERSION"
+  pip3 install --force-reinstall torch==$PYTORCH_VERSION torchaudio==$PYTORCH_VERSION torchvision==$PYTORCH_VERSION --index-url https://download.pytorch.org/whl/nightly/cu128
 else
   echo "Installing PyTorch version from ComfyUI Manager repo..."
   pytorch_line=$(curl -s https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/scripts/install-comfyui-venv-linux.sh | grep 'torch torchvision torchaudio')
--- attn.bash
+++ attn.bash
@@ -2,11 +2,15 @@ echo "== Activating virtua lenv"
 source /comfy/mnt/venv/bin/activate
 
 if [ "$PYTORCH_VERSION" = "nightly" ]; then
-  echo "== Installing nightly torch from https://download.pytorch.org/whl/nightly/cu128"
+  echo "Installing nightly PyTorch..."
   pip3 install --upgrade --pre torch torchaudio torchvision --index-url https://download.pytorch.org/whl/nightly/cu128
 elif [ -n "$PYTORCH_VERSION" ]; then
   echo "== Installing torch version $PYTORCH_VERSION"
   pip3 install --force-reinstall torch==$PYTORCH_VERSION torchaudio==$PYTORCH_VERSION torchvision==$PYTORCH_VERSION --index-url https://download.pytorch.org/whl/nightly/cu128
+else
+  echo "Installing PyTorch version from ComfyUI Manager repo..."
+  pytorch_line=$(curl -s https://raw.githubusercontent.com/ltdrdata/ComfyUI-Manager/main/scripts/install-comfyui-venv-linux.sh | grep 'torch torchvision torchaudio')
+  eval "$pytorch_line"
 fi
 
 # Install whl files under /builds directory
