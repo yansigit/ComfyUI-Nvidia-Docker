89a842f8f765d27c94f61a0655e42585e8aca48f
Aiden Yoon <44089734+yansigit@users.noreply.github.com>
2025-03-07 23:19:53 -0700

    add pytorch version override

--- attn.bash
+++ attn.bash
@@ -4,6 +4,9 @@ source /comfy/mnt/venv/bin/activate
 if [ "$PYTORCH_VERSION" = "nightly" ]; then
   echo "== Installing nightly torch from https://download.pytorch.org/whl/nightly/cu128"
   pip3 install --upgrade --pre torch torchaudio torchvision --index-url https://download.pytorch.org/whl/nightly/cu128
+elif [ -n "$PYTORCH_VERSION" ]; then
+  echo "== Installing torch version $PYTORCH_VERSION"
+  pip3 install --force-reinstall torch==$PYTORCH_VERSION torchaudio==$PYTORCH_VERSION torchvision==$PYTORCH_VERSION --index-url https://download.pytorch.org/whl/nightly/cu128
 fi
 
 # Install whl files under /builds directory
