f31ad60a453dc8108aea3bc6080a7a2fba28c1b1
Aiden Yoon <44089734+yansigit@users.noreply.github.com>
2025-03-08 00:17:38 -0700

    exclude torchaudio when installing pytorch nightly

--- attn-build.bash
+++ attn-build.bash
@@ -9,7 +9,8 @@ pip3 install setuptools wheel packaging
 
 if [ "$PYTORCH_VERSION" = "nightly" ]; then
   echo "Installing nightly PyTorch..."
-  pip3 install --upgrade --pre torch torchaudio torchvision --index-url https://download.pytorch.org/whl/nightly/cu128
+  # pip3 install --upgrade --pre torch torchaudio torchvision --index-url https://download.pytorch.org/whl/nightly/cu128
+  pip3 install --upgrade --pre torch torchvision --index-url https://download.pytorch.org/whl/nightly/cu128
 elif [ -n "$PYTORCH_VERSION" ]; then
   echo "== Installing torch version $PYTORCH_VERSION"
   pip3 install --force-reinstall torch==$PYTORCH_VERSION torchaudio==$PYTORCH_VERSION torchvision==$PYTORCH_VERSION --index-url https://download.pytorch.org/whl/nightly/cu128
